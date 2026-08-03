932a7e390cf43831eed893baaafc1f4927930bf5
Kiprop2020 <peterkurui7@gmail.com>
2026-08-03 23:11:48 +0300

    Pulpit sermon API added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log("Pulpit started successfully.");
+const API_URL = "https://script.google.com/macros/s/AKfycbw0ih9peIYxyE7tswYVkJ38VHuBcAwLHDgtCfSZq74OcQ3ixL8rP0ErutO77iotB1ISgA/exec";
+
+
+async function loadSermons() {
+
+    try {
+
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+
+            throw new Error(
+                `HTTP error: ${response.status}`
+            );
+
+        }
+
+        const sermons = await response.json();
+
+        console.log("Sermons received:", sermons);
+
+    }
+
+    catch (error) {
+
+        console.error(
+            "Unable to load sermons:",
+            error
+        );
+
+    }
+
+}
+
+
+loadSermons();
